9750a6c5661e95c6f268c6b8f794e0423b9f2632
namunanamu <namunanamu76@gmail.com>
2026-03-24 21:28:15 +0900

    refactor: 得点ゾーン3の得点変更部分を関数化
    branxch#refactor/code-structure

--- src/game.js
+++ src/game.js
@@ -18,6 +18,7 @@ import { numPlayers } from "./helpers/screen/option/numPlayers.js";
 import { pointZoneWidth } from "./helpers/screen/option/pointZoneWidth.js";
 import { pointZone1 } from "./helpers/screen/option/pointZone1.js";
 import { pointZone2 } from "./helpers/screen/option/pointZone2.js";
+import { pointZone3 } from "./helpers/screen/option/pointZone3.js";
 
 ////// ゲームに必要なパラメータ //////
 let question_number; // 何番目のお題をランダムに選んだか
@@ -76,33 +77,8 @@ export function option(canvas, context){
     // 得点ゾーン２の得点
     let [left_button4, right_button4] = pointZone2(canvas, context);
 
-    // 得点ゾーン２の得点
-    draw_text_on_option("得点ゾーン３の得点", 0.75, canvas, context);
-    draw_number_on_option(points[2], 0.7, 0.75, canvas, context);
-    let triangle_left5 = new Image();
-    let triangle_right5 = new Image();
-    triangle_left5.src = "img/triangle_left.png";
-    triangle_right5.src = "img/triangle_right.png";
-    let left_button5 = new ImageButton(
-        canvas.width * 0.6,     // x座標
-        canvas.height * 0.73,    // y座標
-        canvas.width * 0.1,     // 横幅
-        canvas.height * 0.1,   // 縦幅
-        triangle_left5, // 画像
-    );
-    let right_button5 = new ImageButton(
-        canvas.width * 0.8,     // x座標
-        canvas.height * 0.73,    // y座標
-        canvas.width * 0.1,     // 横幅
-        canvas.height * 0.1,   // 縦幅
-        triangle_right5, // 画像
-    );
-    triangle_left5.onload = function(){
-        left_button5.draw(canvas, context);
-    };
-    triangle_right5.onload = function(){
-        right_button5.draw(canvas, context);
-    };
+    // 得点ゾーン３の得点
+    let [left_button5, right_button5] = pointZone3(canvas, context);
 
     // タイトルに戻るボタンの描画
     let back_to_title_button = new Button(
---
+++ src/helpers/screen/option/pointZone3.js
@@ -0,0 +1,35 @@
+import { draw_text_on_option } from "../../../function.js";
+import { draw_number_on_option } from "../../../function.js";
+import { points } from "../../../game.js";
+import { ImageButton } from "../../../class.js";
+
+export function pointZone3(canvas, context) {
+    draw_text_on_option("得点ゾーン３の得点", 0.75, canvas, context);
+    draw_number_on_option(points[2], 0.7, 0.75, canvas, context);
+    let triangle_left5 = new Image();
+    let triangle_right5 = new Image();
+    triangle_left5.src = "img/triangle_left.png";
+    triangle_right5.src = "img/triangle_right.png";
+    let left_button5 = new ImageButton(
+        canvas.width * 0.6,     // x座標
+        canvas.height * 0.73,    // y座標
+        canvas.width * 0.1,     // 横幅
+        canvas.height * 0.1,   // 縦幅
+        triangle_left5, // 画像
+    );
+    let right_button5 = new ImageButton(
+        canvas.width * 0.8,     // x座標
+        canvas.height * 0.73,    // y座標
+        canvas.width * 0.1,     // 横幅
+        canvas.height * 0.1,   // 縦幅
+        triangle_right5, // 画像
+    );
+    triangle_left5.onload = function(){
+        left_button5.draw(canvas, context);
+    };
+    triangle_right5.onload = function(){
+        right_button5.draw(canvas, context);
+    };
+
+    return [left_button5, right_button5];
+}
