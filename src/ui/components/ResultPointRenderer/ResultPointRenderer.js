70235884ed534ecef77ca6bddda29618a8a24200
namunanamu <namunanamu76@gmail.com>
2026-04-03 14:22:01 +0900

    refactor: 点数ジャッジ周辺のリファクタ。drawResultPointText の導入
    branch#refactor/code-structure

--- src/ui/components/ResultPointRenderer/ResultPointRenderer.js
+++ src/ui/components/ResultPointRenderer/ResultPointRenderer.js
@@ -1,8 +1,7 @@
 import { canvas } from "../../../core/canvas/Canvas.js";
 import { JUDGES } from "../../../game/scoring/judges.js";
-import { gameSettings } from "../../../game/states/gameSettings.js";
 import { drawBackGround, getBackGroundPosX } from "./internal/backGround.js";
-import { getResultPointText, setStyleForText } from "./internal/pointText.js";
+import { drawResultPointText } from "./internal/pointText.js";
 
 class ResultPointRenderer {
 
@@ -11,49 +10,42 @@ class ResultPointRenderer {
     }
 
     draw(context, areas) {
-        let texts = [];
         // 参加プレイヤーが2人の場合
         if(areas.length == 1){
             if(areas[0] == 0){
                 drawBackGround(context, JUDGES.PERFECT.color, getBackGroundPosX({ numPlayers: 2, playerNumber: 1 }));
-                texts[0] = getResultPointText(gameSettings.points[0]);
+                drawResultPointText(context, JUDGES.PERFECT, canvas.getWidth() * 0.5, canvas.getWidth() * 0.1);
             }
             else if(areas[0] == 1){
                 drawBackGround(context, JUDGES.GREAT.color, getBackGroundPosX({ numPlayers: 2, playerNumber: 1 }));
-                texts[0] = getResultPointText(gameSettings.points[1]);
+                drawResultPointText(context, JUDGES.GREAT, canvas.getWidth() * 0.5, canvas.getWidth() * 0.1);
             }
             else if(areas[0] == 2){
                 drawBackGround(context, JUDGES.GOOD.color, getBackGroundPosX({ numPlayers: 2, playerNumber: 1 }));
-                texts[0] = getResultPointText(gameSettings.points[2]);
+                drawResultPointText(context, JUDGES.GOOD, canvas.getWidth() * 0.5, canvas.getWidth() * 0.1);
             }
             else{
-                texts[0] = getResultPointText(0);
+                drawResultPointText(context, JUDGES.MISS, canvas.getWidth() * 0.5, canvas.getWidth() * 0.1);
             }
-            // スタイルの決定
-            setStyleForText(context);
-            context.fillText(texts[0], canvas.getWidth() * 0.5, canvas.getWidth() * 0.1);
         }
         // 参加プレイヤーが3人の場合
         if(areas.length == 2){
             //// プレイヤー1の得点
             if(areas[0] == 0){
                 drawBackGround(context, JUDGES.PERFECT.color, getBackGroundPosX({ numPlayers: 3, playerNumber: 1 }));
-                texts[0] = getResultPointText(gameSettings.points[0]);
+                drawResultPointText(context, JUDGES.PERFECT, canvas.getWidth() * 0.3, canvas.getWidth() * 0.1);
             }
             else if(areas[0] == 1){
                 drawBackGround(context, JUDGES.GREAT.color, getBackGroundPosX({ numPlayers: 3, playerNumber: 1 }));
-                texts[0] = getResultPointText(gameSettings.points[1]);
+                drawResultPointText(context, JUDGES.GREAT, canvas.getWidth() * 0.3, canvas.getWidth() * 0.1);
             }
             else if(areas[0] == 2){
                 drawBackGround(context, JUDGES.GOOD.color, getBackGroundPosX({ numPlayers: 3, playerNumber: 1 }));
-                texts[0] = getResultPointText(gameSettings.points[2]);
+                drawResultPointText(context, JUDGES.GOOD, canvas.getWidth() * 0.3, canvas.getWidth() * 0.1);
             }
             else{
-                texts[0] = getResultPointText(0);
+                drawResultPointText(context, JUDGES.MISS, canvas.getWidth() * 0.3, canvas.getWidth() * 0.1);
             }
-            // 点数をテキストとして描画
-            setStyleForText(context);
-            context.fillText(texts[0], canvas.getWidth() * 0.3, canvas.getWidth() * 0.1);
             // どっちのプレイヤーか描画
             context.fillStyle = "rgb(200, 0, 0)";
             context.font = canvas.getHeight() * 0.03 + "px serif";
@@ -62,22 +54,19 @@ class ResultPointRenderer {
             //// プレイヤー2の得点
             if(areas[1] == 0){
                 drawBackGround(context, JUDGES.PERFECT.color, getBackGroundPosX({ numPlayers: 3, playerNumber: 2 }));
-                texts[1] = getResultPointText(gameSettings.points[0]);
+                drawResultPointText(context, JUDGES.PERFECT, canvas.getWidth() * 0.7, canvas.getWidth() * 0.1);
             }
             else if(areas[1] == 1){
                 drawBackGround(context, JUDGES.GREAT.color, getBackGroundPosX({ numPlayers: 3, playerNumber: 2 }));
-                texts[1] = getResultPointText(gameSettings.points[1]);
+                drawResultPointText(context, JUDGES.GREAT, canvas.getWidth() * 0.7, canvas.getWidth() * 0.1);
             }
             else if(areas[1] == 2){
                 drawBackGround(context, JUDGES.GOOD.color, getBackGroundPosX({ numPlayers: 3, playerNumber: 2 }));
-                texts[1] = getResultPointText(gameSettings.points[2]);
+                drawResultPointText(context, JUDGES.GOOD, canvas.getWidth() * 0.7, canvas.getWidth() * 0.1);
             }
             else{
-                texts[1] = getResultPointText(0);
+                drawResultPointText(context, JUDGES.MISS, canvas.getWidth() * 0.7, canvas.getWidth() * 0.1);
             }
-            // 点数をテキストとして描画
-            setStyleForText(context);
-            context.fillText(texts[1], canvas.getWidth() * 0.7, canvas.getWidth() * 0.1);
             // どっちのプレイヤーか描画
             context.fillStyle = "rgb(0, 0, 200)";
             context.font = canvas.getHeight() * 0.03 + "px serif";
@@ -88,22 +77,19 @@ class ResultPointRenderer {
             //// プレイヤー1の得点
             if(areas[0] == 0){
                 drawBackGround(context, JUDGES.PERFECT.color, getBackGroundPosX({ numPlayers: 4, playerNumber: 1 }));
-                texts[0] = getResultPointText(gameSettings.points[0]);
+                drawResultPointText(context, JUDGES.PERFECT, canvas.getWidth() * 0.17, canvas.getWidth() * 0.1);
             }
             else if(areas[0] == 1){
                 drawBackGround(context, JUDGES.GREAT.color, getBackGroundPosX({ numPlayers: 4, playerNumber: 1 }));
-                texts[0] = getResultPointText(gameSettings.points[1]);
+                drawResultPointText(context, JUDGES.GREAT, canvas.getWidth() * 0.17, canvas.getWidth() * 0.1);
             }
             else if(areas[0] == 2){
                 drawBackGround(context, JUDGES.GOOD.color, getBackGroundPosX({ numPlayers: 4, playerNumber: 1 }));
-                texts[0] = getResultPointText(gameSettings.points[2]);
+                drawResultPointText(context, JUDGES.GOOD, canvas.getWidth() * 0.17, canvas.getWidth() * 0.1);
             }
             else{
-                texts[0] = getResultPointText(0);
+                drawResultPointText(context, JUDGES.MISS, canvas.getWidth() * 0.17, canvas.getWidth() * 0.1);
             }
-            // 点数をテキストとして描画
-            setStyleForText(context);
-            context.fillText(texts[0], canvas.getWidth() * 0.17, canvas.getWidth() * 0.1);
             // どっちのプレイヤーか描画
             context.fillStyle = "rgb(200, 0, 0)";
             context.font = canvas.getHeight() * 0.03 + "px serif";
@@ -112,22 +98,19 @@ class ResultPointRenderer {
             //// プレイヤー2の得点
             if(areas[1] == 0){
                 drawBackGround(context, JUDGES.PERFECT.color, getBackGroundPosX({ numPlayers: 4, playerNumber: 2 }));
-                texts[1] = getResultPointText(gameSettings.points[0]);
+                drawResultPointText(context, JUDGES.PERFECT, canvas.getWidth() * 0.5, canvas.getWidth() * 0.1);
             }
             else if(areas[1] == 1){
                 drawBackGround(context, JUDGES.GREAT.color, getBackGroundPosX({ numPlayers: 4, playerNumber: 2 }));
-                texts[1] = getResultPointText(gameSettings.points[1]);
+                drawResultPointText(context, JUDGES.GREAT, canvas.getWidth() * 0.5, canvas.getWidth() * 0.1);
             }
             else if(areas[1] == 2){
                 drawBackGround(context, JUDGES.GOOD.color, getBackGroundPosX({ numPlayers: 4, playerNumber: 2 }));
-                texts[1] = getResultPointText(gameSettings.points[2]);
+                drawResultPointText(context, JUDGES.GOOD, canvas.getWidth() * 0.5, canvas.getWidth() * 0.1);
             }
             else{
-                texts[1] = getResultPointText(0);
+                drawResultPointText(context, JUDGES.MISS, canvas.getWidth() * 0.5, canvas.getWidth() * 0.1);
             }
-            // 点数をテキストとして描画
-            setStyleForText(context);
-            context.fillText(texts[1], canvas.getWidth() * 0.5, canvas.getWidth() * 0.1);
             // どっちのプレイヤーか描画
             context.fillStyle = "rgb(0, 0, 200)";
             context.font = canvas.getHeight() * 0.03 + "px serif";
@@ -136,22 +119,19 @@ class ResultPointRenderer {
             //// プレイヤー3の得点
             if(areas[2] == 0){
                 drawBackGround(context, JUDGES.PERFECT.color, getBackGroundPosX({ numPlayers: 4, playerNumber: 3 }));
-                texts[2] = getResultPointText(gameSettings.points[0]);
+                drawResultPointText(context, JUDGES.PERFECT, canvas.getWidth() * 0.83, canvas.getWidth() * 0.1);
             }
             else if(areas[2] == 1){
                 drawBackGround(context, JUDGES.GREAT.color, getBackGroundPosX({ numPlayers: 4, playerNumber: 3 }));
-                texts[2] = getResultPointText(gameSettings.points[1]);
+                drawResultPointText(context, JUDGES.GREAT, canvas.getWidth() * 0.83, canvas.getWidth() * 0.1);
             }
             else if(areas[2] == 2){
                 drawBackGround(context, JUDGES.GOOD.color, getBackGroundPosX({ numPlayers: 4, playerNumber: 3 }));
-                texts[2] = getResultPointText(gameSettings.points[2]);
+                drawResultPointText(context, JUDGES.GOOD, canvas.getWidth() * 0.83, canvas.getWidth() * 0.1);
             }
             else{
-                texts[2] = getResultPointText(0);
+                drawResultPointText(context, JUDGES.MISS, canvas.getWidth() * 0.83, canvas.getWidth() * 0.1);
             }
-            // 点数をテキストとして描画
-            setStyleForText(context);
-            context.fillText(texts[2], canvas.getWidth() * 0.83, canvas.getWidth() * 0.1);
             // どっちのプレイヤーか描画
             context.fillStyle = "rgb(0, 200, 0)";
             context.font = canvas.getHeight() * 0.03 + "px serif";
--- src/ui/components/ResultPointRenderer/internal/pointText.js
+++ src/ui/components/ResultPointRenderer/internal/pointText.js
@@ -2,23 +2,36 @@ import { canvas } from "../../../../core/canvas/Canvas.js";
 import { JUDGES } from "../../../../game/scoring/judges.js";
 import { gameSettings } from "../../../../game/states/gameSettings.js";
 
-export const getResultPointText = (point) => {
-    let judge;
-    if (point === gameSettings.points[0]) { judge = JUDGES.PERFECT }
-    else if (point === gameSettings.points[1]) { judge = JUDGES.GREAT }
-    else if (point === gameSettings.points[2]) { judge = JUDGES.GOOD }
-    else { judge = JUDGES.MISS }
+export const drawResultPointText = (context, judge, posX, posY) => {
+    setStyleForText(context);
+    context.fillText(getResultPointText(judge), posX, posY);
+}
 
+const getResultPointText = (judge) => {
+    let point;
     let marks;
-    if (judge === JUDGES.PERFECT) { marks = "！！！" }
-    else if (judge === JUDGES.GREAT) { marks = "！！" }
-    else if (judge === JUDGES.GOOD) { marks = "！" }
-    else { marks = "..." }
+    
+    if (judge === JUDGES.PERFECT) {
+        point = gameSettings.points[0];
+        marks = "！！！";
+    }
+    else if (judge === JUDGES.GREAT) {
+        point = gameSettings.points[1];
+        marks = "！！"
+    }
+    else if (judge === JUDGES.GOOD) {
+        point = gameSettings.points[2];
+        marks = "！"
+    }
+    else {
+        point = 0;
+        marks = "..."
+    }
 
     return point + "点" + marks;
 }
 
-export const setStyleForText = (context) => {
+const setStyleForText = (context) => {
     context.fillStyle = "rgb(0, 0, 0)";
     context.textAlign = "center";
     context.font = canvas.getHeight() * 0.05 + "px serif";
