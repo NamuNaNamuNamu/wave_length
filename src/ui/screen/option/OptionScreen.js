e343a970a13368dc5c6ee0fe56434ce1c9a5e983
namunanamu <namunanamu76@gmail.com>
2026-04-07 18:01:18 +0900

    refactor: enableButtonOption を廃止
    branch#refactor/code-structure

--- src/ui/screen/option/OptionScreen.js
+++ src/ui/screen/option/OptionScreen.js
@@ -1,10 +1,11 @@
+import { dispatchPointerToButtons } from "../../input/dispatchPointerToButtons.js";
+import { pointerInput } from "../../input/PointerInput.js";
 import { buttonBackToTitle } from "./components/buttonBackToTitle.js";
 import { leftButtonNumPlayers, rightButtonNumPlayers } from "./components/buttonsNumPlayers.js";
 import { leftButtonPointZoneGood, rightButtonPointZoneGood } from "./components/buttonsPointZoneGood.js";
 import { leftButtonPointZoneGreat, rightButtonPointZoneGreat } from "./components/buttonsPointZoneGreat.js";
 import { leftButtonPointZonePerfect, rightButtonPointZonePerfect } from "./components/buttonsPointZonePerfect.js";
 import { leftButtonPointZoneSize, rightButtonPointZoneSize } from "./components/buttonsPointZoneSize.js";
-import { enableButtonOption } from "./internal/enableButtonOption.js";
 import { numPlayers } from "./internal/numPlayers.js";
 import { pointZoneGood } from "./internal/pointZoneGood.js";
 import { pointZoneGreat } from "./internal/pointZoneGreat.js";
@@ -12,33 +13,43 @@ import { pointZonePerfect } from "./internal/pointZonePerfect.js";
 import { pointZoneSize } from "./internal/pointZoneSize.js";
 
 class OptionScreen {
+    #buttons;
+    
     constructor() {
+        this.#buttons = [
+            leftButtonNumPlayers,
+            rightButtonNumPlayers,
+            leftButtonPointZoneSize,
+            rightButtonPointZoneSize,
+            leftButtonPointZonePerfect,
+            rightButtonPointZonePerfect,
+            leftButtonPointZoneGreat,
+            rightButtonPointZoneGreat,
+            leftButtonPointZoneGood,
+            rightButtonPointZoneGood,
 
+            buttonBackToTitle
+        ];
     }
 
     draw(context) {
-        leftButtonNumPlayers.draw(context);
-        rightButtonNumPlayers.draw(context);
+        for (let button of this.#buttons) {
+            button.draw(context);
+        }
         numPlayers();
-        leftButtonPointZoneSize.draw(context);
-        rightButtonPointZoneSize.draw(context);
         pointZoneSize();
-        leftButtonPointZonePerfect.draw(context);
-        rightButtonPointZonePerfect.draw(context);
         pointZonePerfect();
-        leftButtonPointZoneGreat.draw(context);
-        rightButtonPointZoneGreat.draw(context);
         pointZoneGreat();
-        leftButtonPointZoneGood.draw(context);
-        rightButtonPointZoneGood.draw(context);
         pointZoneGood();
-
-        // タイトルに戻るボタンの描画
-        buttonBackToTitle.draw(context);
     }
 
     activate() {
-        enableButtonOption();
+        pointerInput.onPointerDown((pointer) => {
+            dispatchPointerToButtons({
+                pointer: pointer,
+                buttons: this.#buttons
+            });
+        });
     }
 }
 
